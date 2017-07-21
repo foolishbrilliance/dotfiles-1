1270e3e6cb88e92294a832c9281ada05b33e3d4f
Joe Chan <joech4n@users.noreply.github.com>
2017-07-21 07:25:47 -0700

    Remove zsh/install.sh, no longer using oh-my-zsh

--- zsh/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# install oh-my-zsh
-if [ ! -d $HOME/.oh-my-zsh ]; then
-  echo "  Installing oh-my-zsh for you."
-  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh 
-fi
