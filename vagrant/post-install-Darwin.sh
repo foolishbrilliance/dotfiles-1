03a662f857c931170a12a7211b9ae5aab89fd2a6
Joe Chan <joech4n@users.noreply.github.com>
2017-08-28 12:13:16 -0700

    remove failing vagrant command

--- vagrant/post-install-Darwin.sh
+++ vagrant/post-install-Darwin.sh
@@ -5,5 +5,13 @@ if ! vagrant plugin list |grep vagrant-aws &>/dev/null; then
 fi
 
 if ! vagrant box list |grep aws &>/dev/null; then
-  vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+  echo 'Install dummy box'
+  # This is broken as of 2017-08-28 with, will debug later when I need to:
+  # ==> box: Box file was not detected as metadata. Adding it directly...
+  # ==> box: Adding box 'dummy' (v0) for provider:
+  #     box: Downloading: https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
+  # An error occurred while downloading the remote file. The error
+  # message, if any, is reproduced below. Please fix this error and try
+  # again.
+  #vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
 fi
