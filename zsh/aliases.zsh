7ed7e81306ce4d6698ab172fff6d4f86724be50f
Joe Chan <joech4n@users.noreply.github.com>
2018-03-13 13:09:08 -0700

    clean up aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,16 +6,32 @@ alias .....='cd ../../../../'
 alias ......='cd ../../../../../'
 alias calc='bc <<<'
 
-function cdf() {
+# cdf - cd into the directory of the selected file
+cdf() {
    local file
    local dir
    file=$(fzf +m -q "$1") && dir=$(dirname "$file") && cd "$dir"
 }
 
+# cdp - cd to selected parent directory (fdr from https://github.com/junegunn/fzf/wiki/examples)
+cdp() {
+  local declare dirs=()
+  get_parent_dirs() {
+    if [[ -d "${1}" ]]; then dirs+=("$1"); else return; fi
+    if [[ "${1}" == '/' ]]; then
+      for _dir in "${dirs[@]}"; do echo $_dir; done
+    else
+      get_parent_dirs $(dirname "$1")
+    fi
+  }
+  local DIR=$(get_parent_dirs $(realpath "${1:-$PWD}") | fzf-tmux --tac)
+  cd "$DIR"
+}
+
 alias cl="fc -e -|pbcopy && echo Copied output of last command to clipboard"
 alias clitxt='curl -sF "upfile=@-" https://clitxt.com |tee /dev/tty | pbcopy'
 
-function edownload() {
+edownload() {
     if [ $# -ne 2 ];
     then
         echo -e "Wrong arguments specified. Usage example:\nedownload https://transfer.sh/nPIxk/test.txt /tmp/test"
@@ -26,14 +42,15 @@ function edownload() {
 alias e2u=epoch2utc
 alias epoch='date +%s'
 
-function epoch2utc() { perl -e "print scalar(localtime($1)) . ' UTC'" } # Usage: epoch2utc 1395249613
+epoch2utc() { perl -e "print scalar(localtime($1)) . ' UTC'" } # Usage: epoch2utc 1395249613
 
-function etransfer() {
+etransfer() {
   cat $1|gpg -ac -o-|curl --progress-bar -X PUT --upload-file "-" https://transfer.sh/test.txt |tee
 }
 
+# fcd - cd to selected directory
 # from https://github.com/junegunn/fzf/wiki/examples | change to fcd to avoid conflict with https://github.com/sharkdp/fd
-function fcd() {
+fcd() {
   local dir
   dir=$(find ${1:-.} -path '*/\.*' -prune \
                   -o -type d -print 2> /dev/null | fzf +m) &&
@@ -42,7 +59,7 @@ function fcd() {
 
 alias fpath='perl -MCwd -e "print Cwd::abs_path shift"' # cpath is another alias, think "canonical path"
 
-function fo() {
+fe() {
   local out file key
   IFS=$'\n' out=($(fzf-tmux --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
   key=$(head -1 <<< "$out")
@@ -50,6 +67,18 @@ function fo() {
   if [ -n "$file" ]; then
     [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
   fi
+}
+
+# Fuzzy view file
+fv() {
+    local file
+    if [[ -e "$1" ]]; then
+        les "$1"
+    else
+        file=$(fzf --query="$1"\
+          --select-1 --exit-0)
+        [ -n "$file" ] && les "$file"
+    fi
 }
 
 # From https://gist.github.com/vlymar/4e43dbeae70ff71f861d
@@ -69,12 +98,12 @@ alias gpm="git push origin master"
 alias irca='growl-irc.sh; ssh -t trundle tmux attach -d'
 alias isearch='ircsearch' # Function to search irssi logs
 
-function j() {
+j() {
     local dir="$(fasd -ld "$@")"
     [[ -d "$dir" ]] && pushd "$dir"
 }
 
-function jd() {
+jd() {
     local dir
     dir=$(find ${1:-*} -path '*/\.*'\
 
@@ -83,7 +112,7 @@ function jd() {
     [ -d "$dir" ] && pushd "$dir"
 }
 
-function jj() {
+jj() {
     local dir
     dir=$(fasd -Rdl |\
         sed "s:$HOME:~:" |\
@@ -97,7 +126,7 @@ alias krbcurl='curl --negotiate -u :'
 listcert() { openssl s_client -showcerts -connect $1:443 </dev/null 2>/dev/null | openssl x509 -inform PEM -text } # Usage: listcert google.com
 alias lscert=listcert
 
-function les() {
+les() {
     ftype=$(pygmentize -N "$1")
     pygmentize -l "$ftype"\
       -f terminal "$1" |\
@@ -117,7 +146,7 @@ alias rs='screen -RD'
 alias sl='ls'
 alias ss="open /System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app" # Start ScreenSaver. This will lock the screen if locking is enabled.
 
-function transfer() { # transfer.sh function from https://gist.github.com/nl5887/a511f172d3fb3cd0e42d#gistcomment-2093683
+transfer() { # transfer.sh function from https://gist.github.com/nl5887/a511f172d3fb3cd0e42d#gistcomment-2093683
     # check arguments
     if [ $# -ne 1 ];
     then
@@ -169,19 +198,9 @@ alias trimw="pbpaste |sed -e 's/[[[:space:]]\r\n]//g' |pbcopy" # Trim all whites
 alias ud='cd ~/dotfiles && git pull; cd -'
 alias utc='date -u'
 
-function v() {
-    local file
-    if [[ -e "$1" ]]; then
-        les "$1"
-    else
-        file=$(fzf --query="$1"\
-          --select-1 --exit-0)
-        [ -n "$file" ] && les "$file"
-    fi
-}
-
 alias vi='vim'
 alias worddiff='git diff --word-diff=color'
+
 alias zpup='cd .zprezto && git pull && git submodule update --init --recursive; cd -' # Update prezto
 
 # fasd
@@ -196,3 +215,10 @@ alias z='fasd_cd -d'     # cd, same functionality as j in autojump
 alias zz='fasd_cd -d -i' # cd with interactive selection
 alias e='f -e $EDITOR' # quick opening files with vim
 alias o='a -e open' # quick opening files with open (OSX)
+
+# fasd & fzf change directory - open best matched file using `fasd` if given argument, filter output of `fasd` using `fzf` else
+ef() {
+    [ $# -gt 0 ] && fasd -f -e ${EDITOR} "$*" && return
+    local file
+    file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && vi "${file}" || return 1
+}
