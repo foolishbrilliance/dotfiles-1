ef63ebc83a89aef4674295e8f4bf6ed958cd99d7
Joe Chan <joech4n@users.noreply.github.com>
2018-05-30 13:24:50 -0700

    add serverless.zshrc

---
+++ aws/serverless.zsh
@@ -0,0 +1,11 @@
+# per https://serverless.com/framework/docs/providers/aws/guide/credentials/
+# Now you can switch per project (/ API) by executing once when you start your project...
+export AWS_REGION=us-west-2
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.zsh
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh ]] && . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.zsh
+
