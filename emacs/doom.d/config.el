6a4b62290a529d1a96190b40265475124b846a5b
Joe Chan <joech4n@users.noreply.github.com>
2018-10-05 23:04:53 -0700

    Updating binds

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -4,3 +4,14 @@
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 18))
 (add-to-list 'ivy-re-builders-alist '(counsel-M-x . ivy--regex-ignore-order))
+
+;; Keybindings
+;; examples at https://github.com/hlissner/doom-emacs/blob/master/modules/private/default/%2Bbindings.el
+
+(map!
+ (:leader
+   :nv [tab] nil
+   :desc "Last buffer"    :n "TAB" #'evil-switch-to-windows-last-buffer
+   (:desc "toggle" :prefix "t"
+          :desc "Truncate lines"           :n "t" #'toggle-truncate-lines)
+   ))
