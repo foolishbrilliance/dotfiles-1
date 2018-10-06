e9232daaaf70ac3ca9c757cd7397b00e2f771e2f
Joe Chan <joech4n@users.noreply.github.com>
2018-10-05 23:04:33 -0700

    Use ivy

--- emacs/doom.d/config.el
+++ emacs/doom.d/config.el
@@ -3,3 +3,4 @@
 ;; Place your private configuration here
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 18))
+(add-to-list 'ivy-re-builders-alist '(counsel-M-x . ivy--regex-ignore-order))
