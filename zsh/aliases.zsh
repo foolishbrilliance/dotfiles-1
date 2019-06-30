7e9306e6caf388ffa5e4971291fc400788f79892
Joe Chan <joech4n@users.noreply.github.com>
2019-06-30 15:03:35 -0700

    update rg alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -155,7 +155,7 @@ alias myip='curl -s checkip.amazonaws.com'
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 isdarwin && alias ql='qlmanage -p "$@" >& /dev/null'
 alias reload!='. ~/.zshrc && echo reloaded .zshrc'
-alias rg="rg --smart-case --follow --hidden -g '!.git'"
+alias rg="rg --hidden --smart-case --follow --no-messages"
 alias rge=ripgrep_fzf_edit_vim
 alias rs='screen -RD'
 alias sl='ls'
