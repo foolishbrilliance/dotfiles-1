9ee9a670e548eaccba075d4dae4619e9c2a57c8c
Joe Chan <joech4n@users.noreply.github.com>
2019-10-16 10:57:31 -0700

    Fix tm to detach all other clients.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -224,9 +224,9 @@ transfer() { # transfer.sh function from https://gist.github.com/nl5887/a511f172
 tm() {
   [[ -n "$TMUX" ]] && change="switch-client" || change="attach-session"
   if [ $1 ]; then
-    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -dt "$1"); return
+    tmux $change -t "$1" 2>/dev/null || (tmux new-session -d -s $1 && tmux $change -t "$1"); return
   fi
-  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) &&  tmux $change -dt "$session" || echo "No sessions found."
+  session=$(tmux list-sessions -F "#{session_name}" 2>/dev/null | fzf --exit-0) && tmux detach-client -s "$session" && tmux $change -t "$session" || echo "No sessions found."
 }
 
 alias tpaste='tmux save-buffer -'
