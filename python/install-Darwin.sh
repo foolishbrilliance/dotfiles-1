ea3005217b5e7998b3729e9ec82857f6f536c61b
Joe Chan <joech4n@users.noreply.github.com>
2021-03-03 23:53:59 -0800

    [python] Update python install.

--- python/install-Darwin.sh
+++ python/install-Darwin.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-if test ! $(which pip)
+if test ! $(which pip3)
 then
   echo "  Installing python/pip for you."
   brew install python       # pip should be installed by brew in python install
-  pip install --upgrade pip # Update pip
 fi
-
